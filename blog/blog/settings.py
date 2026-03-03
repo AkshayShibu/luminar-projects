a4cd0943fab91b140a28455b5ba75fb411317bad
AkshayShibu <akshayshibu215@gmail.com>
2026-03-03 21:15:27 +0530

    import os allowed host

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4ady9-c5vi%3ajvwdf*kh-2y&b8un3i+a*d36c*6_0g#(t2jle
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [.onrender.com]
 
 
 # Application definition
@@ -115,9 +115,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
